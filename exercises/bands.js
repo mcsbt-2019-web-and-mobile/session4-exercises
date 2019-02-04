60ab9fdce6ec2d061f936f9c859ad56ba6a3d0c7
Pepe Garcia <pepe@pepegar.com>
2019-02-04 09:04:09 +0100

    first commit

---
+++ exercises/bands.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const bands = [
+    {
+      name: "The Beatles",
+      instruments: {
+        John: "voice",
+        Paul: "bass",
+        Ringo: "drums",
+        George: "guitar"
+      }
+    },
+    {
+      name: "The Ramones",
+      instruments: {
+        Johnny: "voice",
+        Joey: "guitar",
+        Marky: "drums",
+        DeeDee: "bass"
+      }
+    }
+  ];
---
+++ exercises/operations.js
@@ -0,0 +1,4 @@
+"use strict";
+
+// Create a function operate, that receives an operation and two numbers as parameters.
+// Then call this function with different numbers and operations.
